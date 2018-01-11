a4f18b10c7a7e4a97d944b59f869a6530203eefd
Azrul Aziz <azrultres@gmail.com>
2018-01-11 19:29:21 +0000

    Added content to page

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,27 +1,47 @@
+// all DOM selector
+const content = document.querySelector(".content");
+const allChannelButton = document.querySelector(".allChannel");
+const onlineChannelButton = document.querySelector(".onlineChannel");
+const offlineChannelButton = document.querySelector(".offlineChannel");
 // list of channels to be retrieved from the API
-let channels = ['freecodecamp', 'ESL_SC2', 'noob2ninjas', 'Ogamingsc2'];
+const channels = ['freecodecamp', 'ESL_SC2', 'noob2ninjas', 'Ogamingsc2'];
 const url = 'https://wind-bow.glitch.me/twitch-api/streams/';
-
+const channelUrl = "https://www.twitch.tv/";
+let online = '';
+let offline = '';
 
 // setup ajax calls & loop thru each channel
-
-let xhr = [];
+const xhr = [];
 
 for (let i = 0; i < channels.length; i++) {
     xhr[i] = new XMLHttpRequest();
     xhr[i].open('GET', url + channels[i], true);
     xhr[i].onload = function() {
         if(this.status === 200) {
-            let twitch = JSON.parse(this.responseText);
+            const twitch = JSON.parse(this.responseText);
             if (twitch.stream !== null) {
-                console.log(twitch.stream.channel.display_name + ": online")
+                online += `<div>
+                            Online - <a href="${channelUrl}${channels[i]}" target="_blank">${channels[i]}</a> : 
+                            ${twitch.stream.game} ${twitch.stream.channel.status}
+                           </div>`;
             } else {
-                console.log(channels[i] + ": offline");
+                offline += `<div>Offline - <a href="${channelUrl}${channels[i]}" target="_blank">${channels[i]}</a></div>`;
             } 
+            content.innerHTML = `${online} ${offline}`;
         }   
     };
     xhr[i].send();
 }
 
-console.log(xhr)
+// setup event listener for the toggle channels buttons
+allChannelButton.addEventListener('click', () => {
+    content.innerHTML = `${online} ${offline}`;
+});
 
+onlineChannelButton.addEventListener('click', () => {
+    content.innerHTML = online;
+});
+
+offlineChannelButton.addEventListener('click', () => {
+    content.innerHTML = offline;
+});
