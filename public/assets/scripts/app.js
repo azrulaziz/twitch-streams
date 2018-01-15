a062f66a19dab05c8265b3cdf3f31a3c790cb332
Azrul Aziz <azrultres@gmail.com>
2018-01-15 15:40:55 +0000

    prefinalize

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -3,8 +3,10 @@ const content = document.querySelector(".content");
 const allChannelButton = document.querySelector(".allChannel");
 const onlineChannelButton = document.querySelector(".onlineChannel");
 const offlineChannelButton = document.querySelector(".offlineChannel");
+const searchForm = document.querySelector("#searchForm");
+const inputForm = document.querySelector('.inputForm');
 // list of channels to be retrieved from the API
-const channels = ['freecodecamp', 'ESL_SC2', 'noob2ninjas', 'Ogamingsc2'];
+const channels = ['freecodecamp', 'ESL_SC2', 'noob2ninjas', 'Ogamingsc2', 'cretetion', 'ESLUK', 'Cheddar'];
 const url = 'https://wind-bow.glitch.me/twitch-api/streams/';
 const channelUrl = "https://www.twitch.tv/";
 let online = '';
@@ -14,34 +16,41 @@ let offline = '';
 const xhr = [];
 
 for (let i = 0; i < channels.length; i++) {
-    xhr[i] = new XMLHttpRequest();
-    xhr[i].open('GET', url + channels[i], true);
-    xhr[i].onload = function() {
-        if(this.status === 200) {
-            const twitch = JSON.parse(this.responseText);
-            if (twitch.stream !== null) {
-                online += `<div>
-                            Online - <a href="${channelUrl}${channels[i]}" target="_blank">${channels[i]}</a> : 
-                            ${twitch.stream.game} ${twitch.stream.channel.status}
-                           </div>`;
-            } else {
-                offline += `<div>Offline - <a href="${channelUrl}${channels[i]}" target="_blank">${channels[i]}</a></div>`;
-            } 
-            content.innerHTML = `${online} ${offline}`;
-        }   
-    };
-    xhr[i].send();
+	xhr[i] = new XMLHttpRequest();
+	xhr[i].open('GET', url + channels[i], true);
+	xhr[i].onload = function() {
+		if(this.status === 200) {
+			const twitch = JSON.parse(this.responseText);
+			if (twitch.stream !== null) {
+				online += `<div class="onlineDiv">
+							<a href="${channelUrl}${channels[i]}" target="_blank">${channels[i]}</a> <span class="online"><em>Online</em></span><br />
+							<span class="channelStatus">${twitch.stream.game} ${twitch.stream.channel.status}</span>
+
+						   </div>`;
+			} else {
+				offline += `<div class="offlineDiv"><a href="${channelUrl}${channels[i]}" target="_blank">${channels[i]}</a>
+							<span class="offline"><em>Offline</em></span></div>
+							<span class="channelStatus">No status available</span>`;
+			}
+			content.innerHTML = `${online} ${offline}`;
+		}
+	};
+	xhr[i].send();
 }
 
 // setup event listener for the toggle channels buttons
 allChannelButton.addEventListener('click', () => {
-    content.innerHTML = `${online} ${offline}`;
+	content.innerHTML = `${online} ${offline}`;
 });
 
 onlineChannelButton.addEventListener('click', () => {
-    content.innerHTML = online;
+	content.innerHTML = online;
 });
 
 offlineChannelButton.addEventListener('click', () => {
-    content.innerHTML = offline;
+	content.innerHTML = offline;
 });
+
+
+
+
